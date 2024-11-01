f8193071b077a590e5dbe75a7cbe35666b5f00c4
Dawid Grabek <grabekd37@gmail.com>
2024-11-01 18:36:13 +0100

    finish task 4.4

--- lab4/UserAuth.php
+++ lab4/UserAuth.php
@@ -11,53 +11,52 @@ class UserAuth
 
   public function registerUser($login, $email, $password)
   {
-    // Generate a unique salt
     $salt = bin2hex(random_bytes(16));
-    // Hash the password with the salt
     $passwordHash = hash('sha512', $password . $salt);
 
-    // Insert the user into the database
+    // Encrypt the hash
+    $aes = new Aes();
+    $encryptedHash = $aes->encrypt($passwordHash);
+
     $stmt = $this->pdo->prepare("INSERT INTO user (login, email, password_hash, salt) VALUES (:login, :email, :password_hash, :salt)");
     return $stmt->execute([
       ':login' => $login,
       ':email' => $email,
-      ':password_hash' => $passwordHash,
+      ':password_hash' => $encryptedHash,
       ':salt' => $salt
     ]);
   }
 
   public function loginUser($login, $password)
   {
-    // Retrieve the user's salt and password hash from the database
     $stmt = $this->pdo->prepare("SELECT id, password_hash, salt FROM user WHERE login = :login");
     $stmt->execute([':login' => $login]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user) {
-      // Hash the entered password with the retrieved salt
+      $aes = new Aes();
+      $decryptedHash = $aes->decrypt($user['password_hash']);
       $passwordHash = hash('sha512', $password . $user['salt']);
 
-      // Verify the hashed password matches the stored hash
-      if ($passwordHash === $user['password_hash']) {
-        // Successful login
+      if ($passwordHash === $decryptedHash) {
         $_SESSION['user_id'] = $user['id'];
         return true;
       }
     }
-    return false; // Login failed
+    return false;
   }
 
   public function changePassword($userId, $newPassword)
   {
-    // Generate a new unique salt
     $newSalt = bin2hex(random_bytes(16));
-    // Hash the new password with the new salt
     $newPasswordHash = hash('sha512', $newPassword . $newSalt);
 
-    // Update the user's password and salt in the database
+    $aes = new Aes();
+    $encryptedHash = $aes->encrypt($newPasswordHash);
+
     $stmt = $this->pdo->prepare("UPDATE user SET password_hash = :password_hash, salt = :salt WHERE id = :id");
     return $stmt->execute([
-      ':password_hash' => $newPasswordHash,
+      ':password_hash' => $encryptedHash,
       ':salt' => $newSalt,
       ':id' => $userId
     ]);
